f881c53ae8443b85ddbdd55b72a924042df6a255
Ondrej Sika <ondrej@ondrejsika.com>
2024-01-11 06:28:42 +0100

    feat: Better hostname handling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import os from "os";
 
 const H1 = (props) => (
   <h1
@@ -30,21 +31,31 @@ export default function Index(props) {
         textAlign: "center",
       }}
     >
-        <H1>👋</H1>
-        <H1>{props.counter}</H1>
-        <H2>{props.hostnameApi}</H2>
-        <H2>{props.HOSTNAME}</H2>
+      <H1>👋</H1>
+      <H1>{props.counter}</H1>
+      {props.hostnameApi === props.HOSTNAME ? (
+        <>
+          <H2>{props.HOSTNAME} (frontend)</H2>
+          <H2>{props.hostnameApi} (backend)</H2>
+        </>
+      ) : (
+        <>
+          <H2>{props.HOSTNAME}</H2>
+          <H2>{props.hostnameApi}</H2>
+        </>
+      )}
     </div>
   );
 }
 
 export async function getServerSideProps() {
+  const HOSTNAME = os.hostname();
   var API_ORIGIN = process.env.API_ORIGIN || "http://127.0.0.1";
   const res = await fetch(API_ORIGIN + `/api/counter`);
   const data = await res.json();
   return {
     props: {
-      HOSTNAME: process.env.HOSTNAME || "",
+      HOSTNAME: HOSTNAME,
       counter: data.counter,
       hostnameApi: data.hostname,
     },
